831edcdd895b4393cd9a58e9b169eeb27de27f1e
daniel.villa <daniel.villa@utp.edu.co>
2024-06-20 10:38:52 -0500

    cambios que nunca van a ver

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class WpReferenciasdisponibles(models.Model):
     referenciaunica = models.CharField(max_length=53, blank=True, null=True)
@@ -11,7 +12,9 @@ class WpReferenciasdisponibles(models.Model):
     descsubgrupo = models.CharField(max_length=50, blank=True, null=True)
     grupo = models.CharField(max_length=2, blank=True, null=True)
     subgrupo = models.CharField(max_length=5, blank=True, null=True)
-    tiquete = models.DecimalField(max_digits=28, decimal_places=2, blank=True, null=True)
+    tiquete = models.DecimalField(
+        max_digits=28, decimal_places=2, blank=True, null=True
+    )
     composicion = models.CharField(max_length=50, blank=True, null=True)
     colormp = models.CharField(max_length=50, blank=True, null=True)
     diseño = models.CharField(max_length=50, blank=True, null=True)
@@ -21,35 +24,38 @@ class WpReferenciasdisponibles(models.Model):
 
     class Meta:
         managed = False  # Created from a view. Don't remove.
-        db_table = 'wp_referenciasdisponibles'
+        db_table = "wp_referenciasdisponibles"
 
 
 class WpFotosCatalago(models.Model):
-    referenciaunica = models.CharField(max_length=53, blank=True, null=True) 
+    referenciaunica = models.CharField(max_length=53, blank=True, null=True)
     ruta = models.CharField(max_length=1000, blank=True, null=True)
 
 
 class WpDisponibles(models.Model):
     bodega = models.CharField(max_length=9, blank=True, null=True)
-    referenciaunica = models.CharField(max_length=53,  blank=True, null=True)
-    referencia = models.CharField(max_length=11,  blank=True, null=True)
-    codcolor = models.CharField(max_length=2,  blank=True, null=True)
+    referenciaunica = models.CharField(max_length=53, blank=True, null=True)
+    referencia = models.CharField(max_length=11, blank=True, null=True)
+    codcolor = models.CharField(max_length=2, blank=True, null=True)
     consecutivo = models.IntegerField()
-    desc_referencia = models.CharField(max_length=50,  blank=True, null=True)
-    descgrupo = models.CharField(max_length=50,  blank=True, null=True)
-    descsubgrupo = models.CharField(max_length=50,  blank=True, null=True)
-    grupo = models.CharField(max_length=2,  blank=True, null=True)
-    subgrupo = models.CharField(max_length=5,  blank=True, null=True)
-    tiquete = models.DecimalField(max_digits=28, decimal_places=2, blank=True, null=True)
-    composicion = models.CharField(max_length=50,  blank=True, null=True)
-    colormp = models.CharField(max_length=50,  blank=True, null=True)
-    diseño = models.CharField(max_length=50,  blank=True, null=True)
-    concepto = models.CharField(max_length=50,  blank=True, null=True)
-    tallas = models.CharField(max_length=4000,  blank=True, null=True)
-    silueta = models.CharField(max_length=100,  blank=True, null=True)
+    desc_referencia = models.CharField(max_length=50, blank=True, null=True)
+    descgrupo = models.CharField(max_length=50, blank=True, null=True)
+    descsubgrupo = models.CharField(max_length=50, blank=True, null=True)
+    grupo = models.CharField(max_length=2, blank=True, null=True)
+    subgrupo = models.CharField(max_length=5, blank=True, null=True)
+    tiquete = models.DecimalField(
+        max_digits=28, decimal_places=2, blank=True, null=True
+    )
+    composicion = models.CharField(max_length=50, blank=True, null=True)
+    colormp = models.CharField(max_length=50, blank=True, null=True)
+    diseño = models.CharField(max_length=50, blank=True, null=True)
+    concepto = models.CharField(max_length=50, blank=True, null=True)
+    tallas = models.CharField(max_length=4000, blank=True, null=True)
+    silueta = models.CharField(max_length=100, blank=True, null=True)
     coleccion = models.CharField(max_length=150, blank=True, null=True)
-    ruta = models.CharField(max_length=1000,  blank=True, null=True)
+    ruta = models.CharField(max_length=1000, blank=True, null=True)
+    color = models.CharField(max_length=500, blank=True, null=True)
 
     class Meta:
         managed = False  # Created from a view. Don't remove.
-        db_table = 'wp_disponibles'
+        db_table = "wp_disponibles"
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse,HttpResponseRedirect,JsonResponse
-from django.shortcuts import get_object_or_404, render,redirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import get_object_or_404, render, redirect
 from catalogo.models import *
 from django.db.models import Q
 from django.conf import settings
-from django.contrib.auth.models import User,Permission,ContentType,Group
+from django.contrib.auth.models import User, Permission, ContentType, Group
 from django.contrib.auth.decorators import login_required
 import datetime
 from django.contrib import messages
@@ -11,90 +11,247 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
+
 class CustomDict(dict):
     def __missing__(self, key):
-        return '04'
+        return "04"
+
 
 # Crear un diccionario personalizado
-mi_diccionario = CustomDict({
-    'pereira': '04',
-    'americas': 'PA',
-    'tulua': 'TU',
-    'palmira': 'PL',
-    'pasto': 'UP',
-})
+mi_diccionario = CustomDict(
+    {
+        "pereira": "04",
+        "americas": "PA",
+        "tulua": "TU",
+        "palmira": "PL",
+        "pasto": "UP",
+    }
+)
+
 
 def catalogo(request):
-    return render(request,'inicio_navidad.html')
+    return render(request, "inicio.html")
 
-def lista(request, almacen):   
-    return render(request,'base_navidad.html' ,{'almacen':almacen})
+
+def lista(request, almacen):
+    return render(request, "base.html", {"almacen": almacen})
+
 
 def camisas(request, almacen):
-    print(mi_diccionario[almacen])
-    referencias= WpDisponibles.objects.filter(grupo='01', bodega = mi_diccionario[almacen])
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            grupo="01", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            grupo="01", bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
+
 
 def pantalones(request, almacen):
-    referencias= WpDisponibles.objects.filter(grupo='02', bodega = mi_diccionario[almacen])
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            grupo="02", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            grupo="02", bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
 
 
 def jeans(request, almacen):
-    referencias= WpDisponibles.objects.filter(grupo='35', bodega = mi_diccionario[almacen])
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            grupo="03", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            grupo="35", bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''    
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
 
 
-def camisetas(request , almacen):
-    referencias= WpDisponibles.objects.filter(grupo__in=['03','60','30'], bodega = mi_diccionario[almacen])
+def camisetas(request, almacen):
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            grupo="60", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            grupo__in=["03", "60", "30"], bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
+
 
 def bermudas(request, almacen):
-    referencias= WpDisponibles.objects.filter(grupo='04', bodega = mi_diccionario[almacen])
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            grupo="04", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            grupo="04", bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
+
 
 def calzados(request, almacen):
-    referencias= WpDisponibles.objects.filter(grupo__in=['10','1A','1L','70'], bodega = mi_diccionario[almacen])
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            grupo="05", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            grupo__in=["10", "1A", "1L", "70"], bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
+
 
 def blazers(request, almacen):
-    referencias= WpDisponibles.objects.filter(grupo__in=['21','18'], bodega = mi_diccionario[almacen])
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            grupo="06", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            grupo__in=["21", "18"], bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
+
 
 def vestidos(request, almacen):
-    referencias= WpDisponibles.objects.filter(grupo='22', bodega = mi_diccionario[almacen])
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            grupo="07", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            grupo="22", bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
+
 
 def otros(request, almacen):
-    referencias= WpDisponibles.objects.filter(Q(grupo='CRR'), bodega = mi_diccionario[almacen])
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            grupo="08", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            Q(grupo="CRR"), bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
+
 
 def cubaveras(request, almacen):
-    referencias= WpDisponibles.objects.filter(subgrupo='0118',  bodega = mi_diccionario[almacen])
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            subgrupo="0118", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            subgrupo="0118", bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
+
 
 def buzos(request, almacen):
-    referencias= WpDisponibles.objects.filter(subgrupo='6003', bodega = mi_diccionario[almacen])
+    if request.method == "POST":
+        color = request.POST.get("color")
+        referencias = WpDisponibles.objects.filter(
+            subgrupo="6001", bodega=mi_diccionario[almacen], color=color
+        )
+    else:
+        referencias = WpDisponibles.objects.filter(
+            subgrupo="6001", bodega=mi_diccionario[almacen]
+        )
     for referencia in referencias:
-        referencia.tallas_format = referencia.tallas.split('-') if referencia.tallas else ''
-    return render(request,'referencias.html',{'referencias':referencias})
+        referencia.tallas_format = (
+            referencia.tallas.split("-") if referencia.tallas else ""
+        )
+    colores = WpDisponibles.objects.all().values("color").distinct()
+    return render(
+        request, "referencias.html", {"referencias": referencias, "colores": colores}
+    )
